5c67d327fd46f63e90235a99d86604e0d47ae834
Thais Alessandra Franzen dos Santos <141414499+franzenthais@users.noreply.github.com>
2023-08-11 08:18:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background("pink");
+}
+
+function draw() {
+  
+  
+stroke("white");  
+fill("black");
+
+  if(mouseIsPressed){
+   rect(mouseX,mouseY,30,30); 
+  }
+}
